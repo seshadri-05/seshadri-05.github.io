bce7bd0a9804fa96dbd247c648b670d736167e11
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-10-12 17:25:05 +0530

    feat: easter egg

--- themes/meloxy/assets/js/main.js
+++ themes/meloxy/assets/js/main.js
@@ -1,4 +1,18 @@
-const hue = Math.random() * 360;
-const color = localStorage.getItem("color") ?? `oklch(0.664 0.1851 ${hue})`;
-localStorage.setItem("color", color);
+function newRandomHue() {
+  const hue = Math.random() * 360;
+  return `oklch(0.664 0.1851 ${hue})`;
+}
+
+function setNewRandomHue() {
+  const color = newRandomHue();
+  document.documentElement.style.setProperty('--source', color);
+  localStorage.setItem("color", color);
+}
+
+const color = localStorage.getItem("color") ?? newRandomHue();
 document.documentElement.style.setProperty('--source', color);
+
+document.addEventListener("DOMContentLoaded", function() {
+  const h1 = document.querySelector("h1")
+  h1.addEventListener('click', setNewRandomHue)
+})
